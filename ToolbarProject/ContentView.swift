0e70090fa0d01594908fd888bef1d8d8c3a32854
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-18 15:37:23 +0200

    Initial Commit

---
+++ ToolbarProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToolbarProject
+//
+//  Created by Esteban Calvete Iglesias on 18/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Esteban Calvete Iglesias on 18/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
